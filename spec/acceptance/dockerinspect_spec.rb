424d45208e773ca7bf7e2214a994e7111e26aae9
Oliver Wolf <owolf@seibert-media.net>
2016-12-17 16:06:11 +0100

    modified rspec test

--- spec/acceptance/dockerinspect_spec.rb
+++ spec/acceptance/dockerinspect_spec.rb
@@ -11,7 +11,7 @@ describe docker_image 'atlassian-crowd:build' do
   its(['Config.Cmd']) { should include '/usr/local/bin/service' }
   its(['Config.Entrypoint']) { should include '/usr/local/bin/entrypoint' }
 
-  its(['Config.User']) { should match 'daemon' }
+  its(['Config.User']) { should match 'crowd' }
 
   its(['Config.Env']) { should include 'JAVA_VERSION_MAJOR=8' }
   its(['Config.Env']) { should include 'CROWD_INST=/opt/atlassian/crowd' }
